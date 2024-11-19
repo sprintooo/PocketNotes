f8c9236b276094bb9f75b3d496eba509fc0ff34d
Himanshu Patidar <himanshupatidar622@gmail.com>
2024-11-19 16:23:06 +0530

    refact: material theme customized

--- app/src/main/java/com/sprinto/pocketnotes/ui/theme/Theme.kt
+++ app/src/main/java/com/sprinto/pocketnotes/ui/theme/Theme.kt
@@ -1,26 +1,29 @@
 package com.sprinto.pocketnotes.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF5277E0),
+    //primaryVariant = Purple700,
+    secondary = Color(0xFF5277E0),
+
+    background = Color(0xFF1B1B1B),
+    surface = Color(0xFF212121),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onSurface = Color.White,
+    onBackground = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Purple500,
+    //primaryVariant = Purple700,
+    secondary = Teal200
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,23 +39,18 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun PocketNotesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
